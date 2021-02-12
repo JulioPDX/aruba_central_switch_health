e5d4a8f51d2f022146edf0ee19713fb4dd8d2b76
JulioPDX <juliop503@gmail.com>
2021-02-12 10:16:39 -0800

    pushing up syntax fixes after running black

--- switch_health.py
+++ switch_health.py
@@ -5,36 +5,24 @@ from rich.table import Table
 from pycentral.base import ArubaCentralBase
 
 # Creating dependencies for API calls, from Aruba team docs
+central_info = {"base_url": "aruba_api_url", "token": {"access_token": "some_token"}}
 
-central_info = {
-    "base_url": "aruba_api_url",
-    "token": {
-        "access_token": "some_token"
-    }
-}
-
-central = ArubaCentralBase(central_info=central_info, 
-ssl_verify=True)
+central = ArubaCentralBase(central_info=central_info, ssl_verify=True)
 
 # Setting parameters for specific call
 apiPath = "/platform/device_inventory/v1/devices"
 apiMethod = "GET"
-apiParams = {
-    "limit": 30,
-    "offset": 0,
-    "sku_type": "MAS"
-}
+apiParams = {"limit": 30, "offset": 0, "sku_type": "MAS"}
 
 # Creating empty list, we will add to this list in a sec
 switches = []
 
 # Actual API call here
-device_resp = central.command(apiMethod=apiMethod, apiPath=apiPath,
-apiParams=apiParams)
+device_resp = central.command(apiMethod=apiMethod, apiPath=apiPath, apiParams=apiParams)
 
 # Appending to list we mentioned earlier
-for device in device_resp['msg']['devices']:
-    switches.append(device['serial'])
+for device in device_resp["msg"]["devices"]:
+    switches.append(device["serial"])
 
 # Creating table, thanks to https://calmcode.io/rich/tables.html
 device_table = Table(title="Aruba Switch Health")
@@ -46,24 +34,27 @@ device_table.add_column("Model", justify="center", style="dodger_blue2")
 for serial in switches:
     apiPath = f"/monitoring/v1/switches/{serial}"
     apiMethod = "GET"
-    apiParams = {
-        "serial": f"{serial}"
-    }
+    apiParams = {"serial": f"{serial}"}
     # Saving each device response to device_resp variable
     # If statements are just checking a key and then adding rows to table
-    device_resp = central.command(apiMethod=apiMethod, apiPath=apiPath,
-    apiParams=apiParams)
-    if 'status' in device_resp['msg'].keys():
-        if device_resp['msg']['status'] != 'Up':
+    device_resp = central.command(
+        apiMethod=apiMethod, apiPath=apiPath, apiParams=apiParams
+    )
+    if "status" in device_resp["msg"].keys():
+        if device_resp["msg"]["status"] != "Up":
             device_table.add_row(
-                f"{device_resp['msg']['switch_type']}", 
-                f":thumbs_down: [red]{device_resp['msg']['status']}[/] :thumbs_down:", 
-                f"{device_resp['msg']['model']}", style='green')
-        elif device_resp['msg']['status'] == 'Up':
+                f"{device_resp['msg']['switch_type']}",
+                f":thumbs_down: [red]{device_resp['msg']['status']}[/] :thumbs_down:",
+                f"{device_resp['msg']['model']}",
+                style="green",
+            )
+        elif device_resp["msg"]["status"] == "Up":
             device_table.add_row(
-                device_resp['msg']['switch_type'], 
-                device_resp['msg']['status'], 
-                device_resp['msg']['model'], style='green')
+                device_resp["msg"]["switch_type"],
+                device_resp["msg"]["status"],
+                device_resp["msg"]["model"],
+                style="green",
+            )
 
 console = Console()
 console.print(device_table)
